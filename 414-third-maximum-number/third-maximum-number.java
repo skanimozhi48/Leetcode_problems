8df7d651d0d1af957bd400f06087ca66bb373f1a
Kanimozhi <kanimozhisit25@gmail.com>
2025-09-14 10:53:22 +0530

    Time: 1 ms (97.90%) | Memory: 43.2 MB (55.68%) - LeetSync

---
+++ 414-third-maximum-number/third-maximum-number.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int thirdMax(int[] nums) {
+      
+       long first = Long.MIN_VALUE;
+        long second = Long.MIN_VALUE;
+         long third = Long.MIN_VALUE;
+         for(int x:nums) {
+            int num =x;
+            if(num == first || num== second || num == third) {
+                continue;
+            } else if(num > first) {
+                third = second;
+                second = first;
+                first = num;
+            } else if( num > second) {
+                third = second;
+                second= num;
+            } else if(num > third) {
+                third = num;
+            }
+         }
+       if(third == Long.MIN_VALUE)
+       return(int) first;
+
+       return (int) third;
+    }
+}
