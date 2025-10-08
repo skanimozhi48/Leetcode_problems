76252f2bf15d334bfe94d57803a91d77785beaca
Kanimozhi <kanimozhisit25@gmail.com>
2025-10-08 11:15:16 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (57.10%) - LeetSync

---
+++ 162-find-peak-element/find-peak-element.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int max = nums[0];
+        int index=0;
+       for(int i =0;i<nums.length;i++) {
+        if(nums[i] > max) {
+            max = nums[i];
+            index = i;
+        }
+       }
+       return index;
+    }
+}
